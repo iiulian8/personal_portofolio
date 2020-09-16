c1f796519c5ca40bd4e4c7ad47eb8e160f47051a
iiulian8 <deceneu@protonmail.com>
2020-09-16 08:08:19 +0100

    123

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar, NavDropdown, NavLink} from "react-bootstrap";
-
+import { Navbar, NavLink } from "react-bootstrap";
 import './Styles/main.scss';
+
+
 
 function App() {
   return (
@@ -18,13 +19,19 @@ function App() {
       <div id="main-container">
         <div id="about">
           <div id="title-div">
-              <a href = "#">Hello, I am Iulian.</a>
-              <h1>I make websites.</h1>
-              <p1>I am a passionate web developer with a desire to improve, innovate and use other big corporate words that don't really mean shit.</p1>
-            </div>
+            <a href="#">Hello, I am Iulian.</a>
+            <h1>I make websites.</h1>
+            <p1>I am a passionate web developer based in UK with a desire to make beautiful and functional websites.</p1>
+          </div>
+          <div id="title-decoration">
+            <hr></hr>
+          </div>
+
+          <div id="worktitle">
+            <h1>My work</h1>
+          </div>
         </div>
 
-        <img src="" ></img>
       </div>
 
     </div>
