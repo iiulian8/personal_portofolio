cd187ec9df0ca47dc3cf9678e8ea9c513ba6dd02
iiulian8 <deceneu@protonmail.com>
2020-09-20 23:41:25 +0100

    123

--- src/Projects.js
+++ src/Projects.js
@@ -9,56 +9,18 @@ function Projects() {
             <div id="projects-title">
                 <h1>My projects:</h1>
             </div>
-            <div id="projects-grid">
-                <div className="projects-cell">
-                    <img src= ""></img>
-                    <a href="#"><div className="project-description">
-                        <p1>Description</p1>
+            
+            <div id = "projects-grid">
+                <div className = "projects-cell">
+                    <img src = ""></img>
+                    <div className = "projects-description">
+                        
                     </div>
-                    </a>
                 </div>
-
-                <div className="projects-cell">
-                    <img src=""></img>
-                    <a href="#"><div className="project-description">
-                        <p1>Description</p1>
-                    </div>
-                    </a>
-                </div>
-
-                <div className="projects-cell">
-                    <img src=""></img>
-                    <a href="#"><div className="project-description">
-                        <p1>Description</p1>
-                    </div>
-                    </a>
-                </div>
-
-                <div className="projects-cell">
-                    <img src=""></img>
-                    <a href="#"><div className="project-description">
-                        <p1>Description</p1>
-                    </div>
-                    </a>
-                </div>
-
-                <div className="projects-cell">
-                    <img src=""></img>
-                    <a href="#"><div className="project-description">
-                        <p1>Description</p1>
-                    </div>
-                    </a>
-                </div>
-
-                <div className="projects-cell">
-                    <img src=""></img>
-                    <a href="#"><div className="project-description">
-                        <p1>Description</p1>
-                    </div>
-                    </a>
-                </div>
-
             </div>
+            
+
+            
         </div>
 
 
