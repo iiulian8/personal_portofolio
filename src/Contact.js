fdc5eefc1a419667538823aba4020ea2cffe72c4
iiulian8 <deceneu@protonmail.com>
2020-09-21 22:53:20 +0100

    123

--- src/Contact.js
+++ src/Contact.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 
 function Contact() {
-    const [first_name, setFirst] = useState('');
-    const [last_name, setLast] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, sendMessage] = useState('');
-
-
-
+ 
     return(
         <div>
-            <p1>Contact Form</p1>
-            <form id = "contact-form">
-                <input type = "text" onChange = {() => setFirst(event.target.value)}></input>
-            </form>
+            <h1>You can find me on:</h1>
+            <div className = "contact-logo">
+
+            </div>
+
+            <div className = "contact-logo">
+                
+            </div>
         </div>
     );
 }
