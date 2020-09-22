7a387a769f9599fd5a3dbb63f0438ef2781caa27
iiulian8 <deceneu@protonmail.com>
2020-09-22 20:20:13 +0100

    123

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,16 +2,14 @@ import React from 'react';
 import './Styles/main.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
 function Nav() {
   return (
-    <nav id = "navbar">
-    <li className = "nav-elm" href = "#">About</li>
-    <li className = "nav-elm" href = "#">Technologies</li>
-    <li className = "nav-elm" href = "#">Projects</li>
-    <li className = "nav-elm" href = "#">Contact</li>
-  </nav>
+    <nav id="navbar">
+      <li className="nav-elm" href="#">About</li>
+      <li className="nav-elm" href="#">Technologies</li>
+      <li className="nav-elm" href="#">Projects</li>
+      <li className="nav-elm" id = "nav-last" href="#">Contact</li>
+    </nav>
   )
 
 }
