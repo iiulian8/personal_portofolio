69659ba1ac441bb8cae38ec830254c01a421932c
iiulian8 <deceneu@protonmail.com>
2020-09-22 17:04:17 +0100

    123

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,53 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import './Styles/main.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, NavLink } from "react-bootstrap";
 
 function Nav() {
 
+    const [width, changeWidth] = useState(window.innerWidth);
+
+    function handleResize() {
+        changeWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+
+
+    function setHamburger() {
+        if (width <= 1080) {
+            return (
+                <Navbar bg="navbar" textAlign="right" className="justify-content-end">
+                    <NavLink className="nav-elm" href="#home">About</NavLink>
+                    <NavLink className="nav-elm" href="#home">Technologies</NavLink>
+                    <NavLink className="nav-elm" href="#home">Projects</NavLink>
+                    <NavLink className="nav-elm" id="last-nav-elm" href="#home">Contact</NavLink>
+                </Navbar>
+            )
+
+        } else {
+            return(
+                <Navbar bg="navbar" textAlign="right" className="justify-content-end">
+                <NavLink className="nav-elm" href="#home">About</NavLink>
+                <NavLink className="nav-elm" href="#home">Technologies</NavLink>
+                <NavLink className="nav-elm" href="#home">Projects</NavLink>
+                <NavLink className="nav-elm" id="last-nav-elm" href="#home">Contact</NavLink>
+            </Navbar>
+            )
+        }
+    }
+
+
+    useEffect(() => {
+        console.log(width);
+    }, [width])
+
+
     return (
-        <Navbar bg="navbar" textAlign="right" className="justify-content-end">
-            <NavLink className="nav-elm" href="#home">About</NavLink>
-            <NavLink className="nav-elm" href="#home">Technologies</NavLink>
-            <NavLink className="nav-elm" href="#home">Projects</NavLink>
-            <NavLink className="nav-elm" id="last-nav-elm" href="#home">Contact</NavLink>
-        </Navbar>
+        <div>
+            {setHamburger()}
+        </div>
     )
 }
 
