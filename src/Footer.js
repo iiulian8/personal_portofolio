b3afc47cf3c3467568113db2c514ce809c1f6cd7
iiulian8 <deceneu@protonmail.com>
2020-09-26 22:57:05 +0100

    123

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,9 @@ import React from 'react';
 function Footer() {
     return (
         <footer id="footer"> 
+        <div id = "footer-made">
        <h1>Made by Iulian Oprea</h1>
+       </div>
         </footer>
     )
 }
