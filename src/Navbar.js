bdd59c981434fbaea11eb55a17ca859a5cd9b361
iiulian8 <deceneu@protonmail.com>
2020-09-30 21:11:10 +0100

    123

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import './Styles/main.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+
 
 function Nav() {
 
+  const [checked, setCheck] = useState(false);
+
+  function handleCheck() {
+    setCheck(prev => !prev);
+  }
+
   return (
     <nav>
-      <input type="checkbox" id="hamburger-check"></input>
+      <input type="checkbox" checked={checked} onClick={handleCheck} id="hamburger-check"></input>
       <label htmlFor="hamburger-check" className="hamburger-button">
         <div className="hamburger-line top"></div>
         <div className="hamburger-line middle"></div>
         <div className="hamburger-line bottom"></div>
       </label>
       <div className="nav-links">
-        <a className="nav-elm" href="#index.html">HOME</a>
-        <a className="nav-elm" href="#work.html">WORK</a>
-        <a className="nav-elm" href="#about.html">ABOUT</a>
-        <a className="nav-elm" href="#contact.html">CONTACT</a>
+        <a className="nav-elm" onClick={handleCheck} href="#index.html">HOME</a>
+        <a className="nav-elm" onClick={handleCheck} href="#work.html">WORK</a>
+        <a className="nav-elm" onClick={handleCheck} href="#about.html">ABOUT</a>
+        <a className="nav-elm" onClick={handleCheck} href="#contact.html">CONTACT</a>
       </div>
     </nav>
   )
