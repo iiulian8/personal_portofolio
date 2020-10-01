8612b172663ceb76a2a96f0f1b8d9e0660e22838
iiulian8 <deceneu@protonmail.com>
2020-10-01 22:30:10 +0100

    123

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { facebook_icon } from './images/import_photos';
-import { } from '@fortawesome/free-solid-svg-icons'
+
 
 function Contact() {
 
@@ -8,7 +8,9 @@ function Contact() {
         <div id="contact-container">
             <h1>Get in touch</h1>
             <a href="#">opreai@roehampton.ac.uk</a>
-            <i class="fab fa-facebook-f"></i>
+            <div className = "icon-cont">
+            <img src = {facebook_icon}></img>
+            </div>
 
 
         </div>
